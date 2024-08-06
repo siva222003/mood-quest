73597cf4068f142dc9de220790186f506d04a7ce
siva222003 <sivapidugu02@gmail.com>
2024-08-07 05:14:53 +0530

    modified server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,12 @@
-import dotenv from "dotenv";
-import path from "path";
-const envPath = path.resolve(__dirname, "../.env"); //irrespective CWD path is fixed
-dotenv.config({ path: envPath });
+import express from "express";
 
-// Import the 'express' module
-import express, { Request, Express } from "express";
-
-// Create an Express application
 const app = express();
+const port = 5000;
 
-// Set the port number for the server
-const port = 3000;  
-
-// Define a route for the root path ('/')
 app.get("/", (req, res) => {
-  // Send a response to the client
-  res.send("Hello, TypeScript + Node.js + Express!");
+  res.send("Hello, TypeScript Node Express!");
 });
-// Start the server and listen on the specified port
+
 app.listen(port, () => {
-  // Log a message when the server is successfully running
-  console.log(`⚡️Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
