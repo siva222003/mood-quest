d49fae8a48e3dcddb7a19e7461d09b4c407f94f2
siva222003 <sivapidugu02@gmail.com>
2024-08-07 05:04:32 +0530

    modified tscnfig

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import express, { Request, Express } from "express";
 const app = express();
 
 // Set the port number for the server
-const port = 3000;
+const port = 3000;  
 
 // Define a route for the root path ('/')
 app.get("/", (req, res) => {
